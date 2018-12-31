cddb030b3848baddcdba69a5c9318d4315c10db6
Cam Arrigo <carrigo3@gmail.com>
2018-12-31 16:13:14 -0500

    Complete FounderButtons app

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel1: UILabel!
+    
+    @IBOutlet weak var messageLabel2: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func googleFounders(_ sender: UIButton) {
+        messageLabel1.text = "Larry Page"
+        messageLabel2.text = "Sergey Brin"
+    }
+    @IBAction func rentTheRunwayFounders(_ sender: UIButton) {
+        messageLabel1.text = "Jennifer Hyman"
+        messageLabel2.text = "Jenny Fleiss"
+    }
+    @IBAction func clearButton(_ sender: UIButton) {
+        messageLabel1.text = ""
+        messageLabel2.text = ""
+    }
+    
 
 
 }
